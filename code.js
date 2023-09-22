59afa50a325a5858be039606182f0c4ce6d2f368
CadeMaynard <143521268+CadeMaynard@users.noreply.github.com>
2023-09-22 08:05:14 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,15 @@
 function divideAndConquerSum(a) {
-    return a;
+    if(a.length == 1){
+        return a[0];
+    } else if(a.length == 0){
+        return 0;
+    } else if(a.length == 2) {
+        return (divideAndConquerSum(a.slice(0, 1)) + divideAndConquerSum(a.slice(1,2))) 
+    } else
+        {
+            
+            return (divideAndConquerSum(a.slice(0, Math.floor(a.length/3))) 
+            + divideAndConquerSum(a.slice(Math.floor(a.length/3), 2 * Math.floor(a.length/3))) 
+            + divideAndConquerSum(a.slice(2 * Math.floor(a.length/3), a.length)));
+        }
 }
